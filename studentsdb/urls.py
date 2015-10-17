4a489e44dc8e16bd11cb41f568fda7c828462e73
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2015-10-17 23:50:58 +0300

    admin model page imrovements, image database linking

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,20 +1,7 @@
-"""studentsdb URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf.urls import patterns, include, url
 
 urlpatterns = [
 	#Journal urls
@@ -36,3 +23,12 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+from .settings import MEDIA_ROOT, DEBUG
+
+
+if DEBUG:
+# serve files from media folder
+		urlpatterns += patterns('',
+				url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+						'document_root': MEDIA_ROOT}))
