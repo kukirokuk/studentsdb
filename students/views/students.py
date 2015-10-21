1f3377956b367da42d178e87b1ee1eacad873293
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2015-10-21 11:36:27 +0300

    pagination rework, mysql migrate

--- students/views/students.py
+++ students/views/students.py
@@ -2,12 +2,33 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.core.paginator import Paginator,EmptyPage, PageNotAnInteger
 
 from ..models import Student
 
 def students_list(request):
 	students = Student.objects.all()
+
+	#try to order students list 
+	order_by = request.GET.get('order_by', '')
+	if order_by in ('id', 'last_name', 'first_name', 'ticket'):
+		students = students.order_by(order_by)
+		if request.GET.get('reverse', '') == '1':
+			students = students.reverse()
 		
+	#paginate students
+	paginator = Paginator(students, 3)	
+	page = request.GET.get('page')
+	try:
+		students = paginator.page(page)
+	except PageNotAnInteger:
+		#If page is not integer, deliever first page
+		students = paginator.page(1)
+	except EmptyPage:
+		#If page is out of range (e.g. 9999), deliver
+		#last page of results.
+		students = paginator(paginator.num_pages)
+
 	return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -78,12 +78,25 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+
+        'HOST': 'localhost',
+
+        'USER': 'students_db_user',
+
+        'PASSWORD': 'password',
+
+        'NAME': 'students-db',
+
     }
-}
+
+ }
+
 
 
 # Internationalization
