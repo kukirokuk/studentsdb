5f605769e08fbfa0ae5fc4e8f280505a066064e3
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2015-11-04 14:25:37 +0200

    cripy form added to contact form

--- students/views/contact_admin.py
+++ students/views/contact_admin.py
@@ -17,18 +17,52 @@ from contact_form.forms import ContactForm
 from studentsdb.settings import ADMIN_EMAIL
 
 class CustomContactForm(ContactForm):
+    # def __init__(self, *args, **kwargs):
+    #     # call original initializator
+    #     super(ContactForm, self).__init__(*args, **kwargs)
+
+           
     def __init__(self, request, *args, **kwargs):
         super(CustomContactForm, self).__init__(request=request, *args, **kwargs)
         fields_keyOrder = ['reason', 'name', 'email', 'body']
+
+        # this helper object allows us to customize form
+        self.helper = FormHelper()
+
+        # form tag attributes
+        self.helper.form_class = 'form-horizontal'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('contact_admin')
+
+        # twitter bootstrap styles
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10' 
+         # form buttons
+        self.helper.add_input(Submit('send_button', u'Надіслати'))
+        
+    email = forms.EmailField(
+        max_length=200,
+        label=u'Ваша Емейл Адреса')
+
+    name = forms.CharField(
+        label=u"Ваше ім'я",
+        max_length=128)
  
+    body = forms.CharField(
+        label=u'Текст повідомлення',
+        max_length=2560,
+            widget=forms.Textarea)
     REASON = (
-        ('support', 'Support'),
-        ('feedback', 'Feedback'),
-        ('delete', 'Account deletion')
+        ('support', u'Тех. підтримка'),
+        ('feedback', u'Відгук'),
+        ('delete', u'Видалення аккаунту')
     )
-    reason = forms.ChoiceField(choices=REASON, label='Reason')
+    reason = forms.ChoiceField(choices=REASON, label=u'Причина')
     template_name = 'contact_form/contact_form.txt'
     subject_template_name = "contact_form/contact_form_subject.txt"
+
 
 class CustomContactFormView(FormView):
     form_class = CustomContactForm
