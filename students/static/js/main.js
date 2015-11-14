3e64dfcb072d67eabf582f6e6da7c2b212e1a7e7
Oleksandr Vinnichuk <oleksanr.v@gmail.com>
2015-11-14 11:52:29 +0200

    students edit new pop up window

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -27,7 +27,28 @@ function initJournal() {
   });
 }
 
+function initGroupSelector() {
+  // look up select element with groups and attach our even handler
+  // on field "change" event
+  $('#group-selector select').change(function(event){
+    // get value of currently selected group option
+    var group = $(this).val();
 
+    if (group) {
+      // set cookie with expiration date 1 year since now;
+      // cookie creation function takes period in days
+      $.cookie('current_group', group, {'path': '/', 'expires': 365});
+    } else {
+      // otherwise we delete the cookie
+      $.removeCookie('current_group', {'path': '/'});
+    }
+
+    // and reload a page
+    location.reload(true);
+
+    return true;
+  });
+}
 
 function initDateFields() {
   $('input.dateinput').datetimepicker({
@@ -37,7 +58,89 @@ function initDateFields() {
   });
 }
 
+function initEditStudentPage() {
+  $('a.student-edit-form-link').click(function(event){
+    var link = $(this);
+    var fink = link.attr('href');
+    $.ajax({
+      'url': link.attr('href'),
+      'dataType': 'html',
+      'type': 'get',
+      'success': function(data, status, xhr){
+        // check if we got successfull response from the server
+        if (status != 'success') {
+          alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+          return false;
+        }
+
+        // update modal window with arrived content from the server
+        var modal = $('#myModal'),
+          html = $(data), form = html.find('#ajax_form');
+        modal.find('.modal-title').html("Форма редагування студента");
+        modal.find('.modal-body').html(form);
+        // init our edit form
+        initEditStudentForm(form, modal);
+
+        // setup and show modal window finally
+        modal.modal({
+          'keyboard': false,
+          'backdrop': false,
+          'show': true
+        });
+      },
+      'error': function(){
+          alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+          return false
+      }
+    });
+
+    return false;
+  });
+}
+
+function initEditStudentForm(form, modal) {
+  // attach datepicker
+  initDateFields();
+
+  // close modal window on Cancel button click
+  form.find('input[name="cancel_button"]').click(function(event){
+    modal.modal('hide');
+    return false;
+  });
+
+  // make form work in AJAX mode
+  form.ajaxForm({
+    'dataType': 'html',
+    'error': function(){
+        alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+        return false;
+    },
+    'success': function(data, status, xhr) {
+      var html = $(data), newform = html.find('#ajax_form');
+
+      // copy alert to modal window
+      modal.find('.modal-body').html(html.find('.alert'));
+
+      // copy form to modal if we found it in server response
+      if (newform.length > 0) {
+        modal.find('.modal-body').append(newform);
+
+        // initialize form fields and buttons
+        initEditStudentForm(newform, modal);
+      } else {
+        // if no form, it means success and we need to reload page
+        // to get updated students list;
+        // reload after 2 seconds, so that user can read success message
+        setTimeout(function(){location.reload(true);}, 500);
+      }
+    }
+  });
+}
+
+
 $(document).ready(function(){
   initJournal();
+  initGroupSelector();
   initDateFields();
+  initEditStudentPage();
 });
